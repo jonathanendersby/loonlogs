8abcbc2cfc3d4a30224744b867f545cc6826b341
Jonathan Endersby <jonathan@endersby.co.za>
2021-05-04 10:54:58 +0200

    PEP8 and comments

--- app.py
+++ app.py
@@ -41,22 +41,28 @@ def log_list():
     for vehicle in vehicles:
         vehicle.show_tracker_link = False
 
+        # If we have coords, get the haversine distance.
         if vehicle.first_latitude and vehicle.first_longitude and vehicle.last_latitude and vehicle.last_longitude:
             l1 = (vehicle.first_latitude, vehicle.first_longitude)
             l2 = (vehicle.last_latitude, vehicle.last_longitude)
             vehicle.flight_distance_km = haversine.haversine(l1, l2)
 
+        # If we have last_heard, calculate the time since we last heard.
         if vehicle.date_last_heard and vehicle.date_last_heard:
             vehicle.flight_time_minutes = (vehicle.date_last_heard - vehicle.date_first_heard).total_seconds() / 60
 
-            if (datetime.utcnow() - vehicle.date_last_heard).total_seconds() < 60*60:  # Have we heard this in the last hour.
+            # Show the tracker link if we've heard this vehicle in the last hour.
+            if (datetime.utcnow() - vehicle.date_last_heard).total_seconds() < 60*60:  # 1 Hour.
                 vehicle.show_tracker_link = True
-                vehicle.tracker_url = utils.get_tracker_url(vehicle.last_latitude, vehicle.last_longitude, vehicle.vehicle_id)
+                vehicle.tracker_url = utils.get_tracker_url(vehicle.last_latitude,
+                                                            vehicle.last_longitude,
+                                                            vehicle.vehicle_id)
 
     return render_template('list.html',
                            vehicles=vehicles,
                            sondehub_url_prefix=settings.sondehub_url_prefix,
-                           system=system)
+                           system=system,
+                           auto_rx_url=settings.auto_rx_url)
 
 
 if __name__ == '__main__':
